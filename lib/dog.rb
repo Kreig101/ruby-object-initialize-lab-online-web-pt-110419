e53304ffe4e139721c909146d11a27cf00c89921
Craig Storer <craigstorer@gmail.com>
2020-01-24 19:37:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,9 @@
 class Dog 
+<<<<<<< HEAD
   def initialize (name, breed = "Mutt")
+=======
+  def initalize(name, breed = "Mutt")
+>>>>>>> ddb13e0e5d928b41798bb5f2d8d76a0b4ade481e
     @name = name 
     @breed = breed 
   end
